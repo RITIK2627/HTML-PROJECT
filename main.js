bfc7d7c7e8b8da81f7bbb846a771089611415908
RITIK2627 <123493402+RITIK2627@users.noreply.github.com>
2023-04-11 19:12:11 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelectorAll('nav a[href^="#"]');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
